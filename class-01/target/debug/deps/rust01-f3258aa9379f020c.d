dc59fac05a8b0420a1352b2d824f6a99a0452ef6
zouyuxuan <1311226127@qq.com>
2023-07-01 07:40:20 +0800

    del target

--- class-01/target/debug/deps/rust01-f3258aa9379f020c.d
+++
@@ -1,6 +0,0 @@
-/Users/zyx/rust/lear-rust/class-01/target/debug/deps/rust01-f3258aa9379f020c: src/main.rs src/print.rs
-
-/Users/zyx/rust/lear-rust/class-01/target/debug/deps/rust01-f3258aa9379f020c.d: src/main.rs src/print.rs
-
-src/main.rs:
-src/print.rs:
--- class-01/target/debug/rust01.d
+++
@@ -1,1 +0,0 @@
-/Users/zyx/rust/lear-rust/class-01/target/debug/rust01: /Users/zyx/rust/lear-rust/class-01/src/main.rs /Users/zyx/rust/lear-rust/class-01/src/print.rs
